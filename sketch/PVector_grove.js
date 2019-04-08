d7bf967ce858d114491a66cbc76458a362cc034f
polycephalus <juamalin@gmail.com>
2019-04-08 22:02:17 +0200

    fixed bounce, added pos getter

--- sketch/PVector_grove.js
+++ sketch/PVector_grove.js
@@ -1,24 +1,31 @@
 let ball;
 let c;
+let currVec;
 
 function setup() {
   // createCanvas(460, 460);
   createCanvas(800, 600);
   background(0);
-  colorMode(HSB, 100, 255, 255);
+  colorMode(HSB, 100, 100, 100);
 
   strokeWeight(2);
+  // noStroke();
   stroke(255);
 
   ball = new Ball();
 }
 
 function draw() {
-  c = frameCount%100;
-  fill(c, 255, 255);
+  c = frameCount%100; //modulo hue pulse
+  fill(c, 100, 100);
+  currVec = ball.position;
   ball.bounce();
   ball.move();
   ball.display();
+
+  // fill(255);
+  // textSize(52);
+  // text(currVec.x , 100, 100);
 }
 
 class Ball {
@@ -26,6 +33,10 @@ class Ball {
     this.pos = createVector(width/2, height/2);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
+  }
+
+  get position(){
+    return this.pos;
   }
 
   move() {
@@ -38,21 +49,31 @@ class Ball {
   //fix bounce
   bounce() {
     if (this.pos.x>width || this.pos.x<0) {
-      this.vel.x *= -1;
+      this.vel.x *= -10;
     }
     if (this.pos.y>height || this.pos.y<0) {
-      this.vel.y *= -1;
+      this.vel.y *= -10;
     }
   }
 
-  // p5.Vector bounce() {
-  //   if (this.pos.x>width || this.pos.x<0) {
-  //     this.vel.x *= -1;
+  //new bounce works anyway
+  // bounce_reset() {
+  //   if (this.pos.x>width) {
+  //     this.pos.x = width;
+  //     this.vel.x *= -10;
   //   }
-  //   if (this.pos.y>height || this.pos.y<0) {
-  //     this.vel.y *= -1;
+  //   if (this.pos.x<0) {
+  //     this.pos.x = 0;
+  //     this.vel.x *= -10;
   //   }
-  //   return this.pos;
+  //   if (this.pos.y>height) {
+  //     this.pos.y = height;
+  //     this.vel.y *= -10;
+  //   }
+  //   if (this.pos.y<0) {
+  //     this.pos.y = 0;
+  //     this.vel.y *= -10;
+  //   }
   // }
 
   display() {
